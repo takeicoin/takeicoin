4d3448b903408c0a83a70228666d864cef7e12a5
takeicoin <contact@takeicoin.com>
2014-03-01 20:24:42 +0000

    TakeiCoin 0.9.0.3

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -38,11 +38,12 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
         ( 0,     uint256("0x00000b4923022e76d07598992d2116f9d965d2727ae1a0ca22e4ae92b2113d76"))
+		(66513, uint256("0x0000000000000dfe73cf058f7bb7c6252b687e5874a5e5b9ca4d65ac49811e31"))
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1391849675, // * UNIX timestamp of last checkpoint block
-        0,     // * total number of transactions between genesis and last checkpoint
+        1393553197, // * UNIX timestamp of last checkpoint block
+        151897,     // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         2880        // * estimated number of transactions per day after checkpoint
     };
