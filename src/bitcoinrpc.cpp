6a113961d346cbbf1e8e703c61db9be1d77614b5
takeicoin <contact@someone-else.org>
2014-07-13 20:40:51 +0000

    send logic from 21000000 to 7515187520

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -90,7 +90,7 @@ void RPCTypeCheck(const Object& o,
 int64 AmountFromValue(const Value& value)
 {
     double dAmount = value.get_real();
-    if (dAmount <= 0.0 || dAmount > 21000000.0)
+    if (dAmount <= 0.0 || dAmount > 7515187520.0)
         throw JSONRPCError(RPC_TYPE_ERROR, "Invalid amount");
     int64 nAmount = roundint64(dAmount * COIN);
     if (!MoneyRange(nAmount))
